f613f22fee9c3536a1ed509721e2b115b1c8e208
Gamer686 <74534832+Gamer686@users.noreply.github.com>
2023-04-20 14:20:02 +0530

    Add files via upload

--- 159/Info-banner.js
+++ 159/Info-banner.js
@@ -8,27 +8,27 @@ AFRAME.registerComponent("info-banner", {
     createBanner: function () {
       postersInfo = {
         "antman": {
-          banner_url: "./assets/ant man.htm",
+          banner_url: "./assets/ant.jpg",
           title: "Antman",
           released_year: " 1962",
           description:
             "Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help his mentor, Dr. Hank Pym, pull off a plan that will save the world."    },
         "spiderman": {
-          banner_url: "./assets/sipderman.htm",
+          banner_url: "./assets/sipdy.jpg",
           title: "Spiderman",
           released_year: "1962",
           description:
             "Spider-Man is a superhero appearing in American comic books published by Marvel Comics. Created by writer-editor Stan Lee and artist Steve Ditko, he first appeared in the anthology comic book Amazing Fantasy #15 (August 1962) in the Silver Age of Comic Books. He has since been featured in movies, television shows, video games, and plays.",
         },
         "Justice-League": {
-          banner_url: "./assets/league.htm",
+          banner_url: "./assets/league.jpg",
           title: "Justice League",
           released_year: "2005",
           description:
             "Fueled by his restored faith in humanity and inspired by Superman's selfless act, Bruce Wayne enlists the help of his new-found ally, Diana Prince, to face an even greater enemy."
         },
         "black-panther": {
-          banner_url: "./assets/black panther.htm",
+          banner_url: "./assets/blackp.jpg",
           title: "Black Panther",
           released_year: "1966",
           description:
